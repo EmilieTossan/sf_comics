8f6cdd00ed442d70b3a1410ed607304ccf212b6c
Émilie Tossan <emilietossan@MacBook-Pro-de-Emilie.home>
2022-01-15 21:32:21 +0100

    forms

--- src/Form/ComicType.php
+++ src/Form/ComicType.php
@@ -3,8 +3,14 @@
 namespace App\Form;
 
 use App\Entity\Comic;
+use App\Entity\Editor;
+use App\Entity\Writer;
+use App\Entity\Licence;
+use App\Entity\Designer;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class ComicType extends AbstractType
@@ -15,10 +21,23 @@ class ComicType extends AbstractType
             ->add('title')
             ->add('description')
             ->add('year')
-            ->add('licence')
-            ->add('editor')
-            ->add('writer')
-            ->add('designer')
+            ->add('licence', EntityType::class, [
+                'class' => Licence::class,
+                'choice_label' => 'name'
+            ])
+            ->add('editor', EntityType::class, [
+                'class' => Editor::class,
+                'choice_label' => 'name'
+            ])
+            ->add('writer', EntityType::class, [
+                'class' => Writer::class,
+                'choice_label' => 'name'
+            ])
+            ->add('designer', EntityType::class, [
+                'class' => Designer::class,
+                'choice_label' => 'name'
+            ])
+            ->add('Enregistrer', SubmitType::class);
         ;
     }
 
